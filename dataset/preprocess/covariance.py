7d4fb2540dd032ca0b930bc7e34ba297b170a156
KIM-KWAN-IL <118953934+KIM-KWAN-IL@users.noreply.github.com>
2023-06-03 13:12:59 +0900

    correlation, covariance

---
+++ dataset/preprocess/covariance.py
@@ -0,0 +1,13 @@
+import seaborn as sn
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+data=pd.read_csv('./data/FireDataset.csv')
+df=pd.DataFrame(data)
+covMatrix = pd.DataFrame.cov(df)
+print (covMatrix)
+
+sn.heatmap(covMatrix, annot = True, fmt='g')
+plt.show()
+
